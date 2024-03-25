8c750584d92578e85a6a7d6d58c9922fd7001963
purupurupkl <huybea123@gmail.com>
2024-03-25 23:42:05 +0700

    new

---
+++ mainCombat.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "gameM.h"
+#include "image.h"
+#include "textureLoader.h"
+#include "entity.h"
+class mainCombat
+{
+public:
+	mainCombat();
+	~mainCombat();
+
+	void loadMedia();
+
+	//handle mouse events
+	void eventHandler(SDL_Event e);
+
+	void update();
+	void render();
+	void clean();
+	static int bgOffset;
+private:
+	image* bg;
+	entity* char1;
+	entity* char2;
+	entity* char3;
+	entity* enemy1;
+	entity* enemy2;
+	entity* enemy3;
+
+};
+
