ac9d9b1a964fc5061f6148ab4bb5acfde4583d16
purupurupkl <huybea123@gmail.com>
2024-03-25 23:40:09 +0700

    source for images

---
+++ entityImage.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+// is this struct really needed?
+//struct entityImage
+//{
+//    //std::string running;
+//    std::string idling;
+//    //std::string attacking;
+//};
+namespace characterImage {
+    const std::string Zeta{
+        "C:\\Users\\HUYBUIAN\\Downloads\\100161_0_100_000.png"
+    };
+    const std::string Cagliostro{
+        "C:\\Users\\HUYBUIAN\\Downloads\\migu.jpg"
+    };
+    const std::string Djeeta{
+        "C:\\Users\\HUYBUIAN\\Downloads\\100161_0_100_007.png"
+    };
+    const std::string Dog{
+        "C:\\Users\\HUYBUIAN\\Desktop\\resources maybe\\dog.jpg"
+    };
+}
