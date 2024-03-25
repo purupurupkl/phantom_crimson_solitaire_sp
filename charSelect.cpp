6baad1953373076258e70ec834c09416f0ac1609
purupurupkl <huybea123@gmail.com>
2024-03-25 23:36:31 +0700

    placeholder for charSelect system

---
+++ charSelect.cpp
@@ -0,0 +1,13 @@
+#include "charSelect.h"
+charSelect::charSelect() {};
+charSelect::~charSelect() {};
+void charSelect::loadMedia() {}
+
+void charSelect::eventHandler(SDL_Event e) {}
+void charSelect::update() {}
+void charSelect::render() {
+
+}
+void charSelect::clean() {
+
+}
---
+++ charSelect.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "gameM.h"
+class charSelect
+{
+public:
+	charSelect();
+	~charSelect();
+
+	void loadMedia();
+
+	//handle mouse events
+	void eventHandler(SDL_Event e);
+
+	void update();
+	void render();
+	void clean();
+};
+
