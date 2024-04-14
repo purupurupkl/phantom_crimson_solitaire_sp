aed52292c99aad0a643bf8dfaca0277e40b21109
purupurupkl <huybea123@gmail.com>
2024-04-14 17:28:19 +0700

    minor

---
+++ level1.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "mainCombat.h"
+class level1 :
+    public mainCombat
+{
+};
+
--- textureLoader.cpp
+++ textureLoader.cpp
@@ -1,6 +1,6 @@
 #include "textureLoader.h"
 SDL_Texture* textureLoader::loadTexture(const char* name) {
-	std::cout << "now loading texture";
+	std::cout << "now loading texture " << std::endl;
 	SDL_Texture* tempTexture;
 	tempTexture = IMG_LoadTexture(gameM::renderer, name);
 	if (tempTexture == NULL) printf("error loading texture: %s", IMG_GetError()); //check
