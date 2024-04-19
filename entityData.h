3c63b0fafa44608e5d5fcb5c05b5291f19bc910e
purupurupkl <huybea123@gmail.com>
2024-04-19 23:08:28 +0700

    modify hp and atk

--- entityData.h
+++ entityData.h
@@ -2,18 +2,20 @@
 #include <string>
 struct entityData {
 	entityData() {};
-	entityData(std::string name, double hp, double atk);
+	entityData(std::string name, int health, int attack);
 	entityData operator=(const entityData& other);
 	std::string name;
-	double hp;
-	double atk;
+	int maxhp;
+	int hp;
+	int maxatk;
+	int atk;
 };
 
 
 namespace characterStats{
-	const entityData DjeetaStats = { "Djeeta", 900, 50};
-	const entityData CagliostroStats = { "Cagliostro", 50, 90};
-	const entityData ZetaStats = { "Zeta", 80, 60};
-	const entityData DogStats = { "Dog", 200, 100};
+	const entityData KkrStats("Djeeta", 900, 50);
+	const entityData CagliostroStats("Cagliostro", 50, 90);
+	const entityData ZetaStats("Zeta", 80, 60);
+	const entityData DogStats("Dog", 100, 50);
 }
 
