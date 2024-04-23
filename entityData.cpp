ee8a708dffc7ac762245d3f40c9f9e2aa08439b2
purupurupkl <huybea123@gmail.com>
2024-04-23 11:02:43 +0700

    map for data

--- entityData.cpp
+++ entityData.cpp
@@ -1,15 +1,18 @@
 #include "entityData.h"
-entityData::entityData(std::string name, int health, int attack) {
+entityData::entityData(std::string name, int health, int attack,int defense) {
 	this->name = name;
 	maxhp = health;
 	maxatk = attack;
+	maxdef = defense;
 	
 }
 entityData entityData::operator=(const entityData& other) {
 	name = other.name;
 	maxhp = other.maxhp;
 	maxatk = other.maxatk;
+	maxdef = other.maxdef;
 	hp = maxhp;
 	atk = maxatk;
+	def = maxdef;
 	return *this;
 }
--- entityData.h
+++ entityData.h
@@ -1,21 +1,27 @@
 #pragma once
 #include <string>
+#include <map>
 struct entityData {
 	entityData() {};
-	entityData(std::string name, int health, int attack);
+	entityData(std::string name, int health, int attack, int defense);
 	entityData operator=(const entityData& other);
 	std::string name;
-	int maxhp;
-	int hp;
-	int maxatk;
-	int atk;
+	int maxhp;  int hp;
+	int maxatk; int atk;
+	int maxdef; int def; 
 };
 
 
 namespace characterStats{
-	const entityData KkrStats("Djeeta", 900, 50);
-	const entityData CagliostroStats("Cagliostro", 50, 90);
-	const entityData ZetaStats("Zeta", 80, 60);
-	const entityData DogStats("Dog", 100, 50);
+	const entityData NianStats("Nian", 300, 50, 20);
+	const entityData SagaStats("Saga", 700, 90, 20);
+	const entityData AprilStats("April", 80, 60, 20);
+	const entityData DogStats("Dog", 100, 50, 10);
+	const std::map<int, entityData> characterData = {
+		{1, NianStats},
+		{2,SagaStats},
+		{3,AprilStats},
+		{4,DogStats}
+	};
 }
 
