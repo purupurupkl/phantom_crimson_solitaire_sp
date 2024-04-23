5d4b4e909188c3789b904cd22f3a035412b031fb
purupurupkl <huybea123@gmail.com>
2024-04-23 11:04:21 +0700

    wrap text

--- writer.cpp
+++ writer.cpp
@@ -7,7 +7,16 @@ writer& writer::get() {
 	return typer;
 }
 void writer::loadText(std::string text, SDL_Color color, SDL_Rect msg, int size) {
-	SDL_Surface* txtSurface = TTF_RenderUTF8_Blended(font, text.c_str(), color);
+	SDL_Surface* txtSurface = TTF_RenderText_Blended(font, text.c_str(), color);
+	TTF_SetFontSize(font, size);
+	SDL_Texture* message = SDL_CreateTextureFromSurface(gameM::renderer, txtSurface);
+	SDL_RenderCopy(gameM::renderer, message, NULL, &msg);
+	SDL_FreeSurface(txtSurface);
+	SDL_DestroyTexture(message);
+	message = NULL;
+}
+void writer::loadTextWrapped(std::string text, SDL_Color color, SDL_Rect msg, int size, Uint32 wrap) {
+	SDL_Surface* txtSurface = TTF_RenderText_Blended_Wrapped(font, text.c_str(), color, wrap);
 	TTF_SetFontSize(font, size);
 	SDL_Texture* message = SDL_CreateTextureFromSurface(gameM::renderer, txtSurface);
 	SDL_RenderCopy(gameM::renderer, message, NULL, &msg);
