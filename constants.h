f3185e3433345ebe1110a36ac8d4da977175f639
purupurupkl <huybea123@gmail.com>
2024-04-26 17:24:16 +0700

    fix

--- constants.h
+++ constants.h
@@ -2,7 +2,3 @@
 #pragma once
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 1000;
-
-
-
-
--- ending.cpp
+++ ending.cpp
@@ -3,14 +3,14 @@
 #include "allyLoader.h"
 #include "writer.h"
 void ending::loadMedia() {
+	std::cout << "loading ending media " << std::endl;
 	if (gameM::won == true) bg = IMG_LoadTexture(gameM::renderer, "resources\\winner.png");
 	else bg = IMG_LoadTexture(gameM::renderer, "resources\\loser.jpg");
-	restart = false;
 };
 void ending::eventHandler(SDL_Event e) {
 	if (e.type == SDL_KEYDOWN) {
-		if (e.key.keysym.sym == SDLK_SPACE) {
-			restart = true;
+		if (e.key.keysym.sym == SDLK_x) {
+			gameM::flag = true;
 		}
 		if (e.key.keysym.sym == SDLK_q) {
 			gameM::quit = true;
@@ -24,18 +24,20 @@ void ending::update(){
 void ending::render() {
 	SDL_RenderClear(gameM::renderer);
 	SDL_RenderFillRect(gameM::renderer, NULL);
+	SDL_RenderCopy(gameM::renderer, bg, NULL, NULL);
 	if(gameM::won == true) 
-		writer::get().loadTextWrapped("you won this very short and bad game!!!", { 0xFF,0xFF,0xFF,0xFF }, { 100,100,200,200 }, 20, 200);
+		writer::get().loadText("you won this very short and bad game!!!", { 0x00,0x00,0x00,0xFF }, { 70,100,400,60 }, 15);
 	else
-		writer::get().loadTextWrapped("you cannot pass this very short and bad game???", { 0xFF,0xFF,0xFF,0xFF }, { 100,100,200,200 }, 20, 200);
-	writer::get().loadTextWrapped("press spacebar to play again or press Q to quit", { 0xFF,0xFF,0xFF,0xFF }, { 200,600,200,200 }, 20, 200);
+		writer::get().loadText("you cannot pass this very short and bad game???", { 0x00,0x00,0x00,0xFF }, { 70,100,400,60 }, 15);
+	writer::get().loadTextWrapped("press x to play again or press Q to quit", { 0x00,0x00,0x00,0xFF }, { 200,900,200,100 }, 15, 200);
 	SDL_RenderPresent(gameM::renderer);
 }
 void ending::clean() {
-	if (restart == true) {
-		gameM::flag = true;
-		gameM::current == gameM::stage1;
+	if (gameM::flag == true) {
+		gameM::current = gameM::main_menu;
 		allyLoader::get().reset();
+		SDL_DestroyTexture(bg);
+		bg = NULL;
 	}
 	
 };
